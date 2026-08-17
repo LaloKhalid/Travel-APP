304c87792fc3d1480758bb7b85aefd13f8249bb1
LarebKhalid <lareb.usman@yahoo.com>
2026-08-17 22:53:30 +0200

    Fix REST Countries API integration

--- src/hooks/useCountries.tsx
+++ src/hooks/useCountries.tsx
@@ -1,21 +1,77 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const API_URL = "https://restcountries.com/v3.1/all?fields=name,region,capital,flags,cca2,cca3,capitalInfo,latlng"
+const API_URL = "https://api.restcountries.com/countries/v5";
+const API_KEY = import.meta.env.VITE_REST_COUNTRIES_API_KEY;
 
+export function useCountries() {
+  return useQuery({
+    queryKey: ["countries"],
+    queryFn: async () => {
+      const { data } = await axios.get(API_URL, {
+        headers: {
+          Authorization: `Bearer ${API_KEY}`,
+        },
+        params: {
+          limit: 100,
+        },
+      });
 
+      return data.data.objects.map((country: any) => ({
+        name: {
+          common: country.names?.common ?? "",
+          official: country.names?.official ?? "",
+        },
 
+        region: country.region ?? "",
+        subregion: country.subregion ?? "",
 
-export function useCountries(){
+        capital: country.capitals?.map((c: any) => c.name) ?? [],
 
-    return useQuery({
-        queryKey:["countries"],
-        queryFn:async() => {
-            const { data } = await axios.get(API_URL);
-            return data;
-        }
-    })
+        flags: {
+          png: country.flag?.url_png ?? "",
+          svg: country.flag?.url_svg ?? "",
+        },
 
+        cca2: country.codes?.alpha_2 ?? "",
+        cca3: country.codes?.alpha_3 ?? "",
 
-  
+        capitalInfo: {
+          latlng: country.capitals?.[0]?.coordinates
+            ? [
+                country.capitals[0].coordinates.lat,
+                country.capitals[0].coordinates.lng,
+              ]
+            : [],
+        },
+
+        latlng: country.coordinates
+          ? [country.coordinates.lat, country.coordinates.lng]
+          : [],
+
+        population: country.population ?? 0,
+
+        languages: country.languages
+          ? Object.fromEntries(
+              country.languages.map((lang: any, index: number) => [
+                `lang${index}`,
+                lang.name,
+              ])
+            )
+          : {},
+
+        currencies: country.currencies
+          ? Object.fromEntries(
+              country.currencies.map((currency: any) => [
+                currency.code,
+                {
+                  name: currency.name,
+                  symbol: currency.symbol,
+                },
+              ])
+            )
+          : {},
+      }));
+    },
+  });
 }
--- src/hooks/useCountry.tsx
+++ src/hooks/useCountry.tsx
@@ -1,22 +1,87 @@
-// src/hooks/useCountry.tsx
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const BASE = "https://restcountries.com/v3.1/alpha";
+const API_URL = "https://api.restcountries.com/countries/v5";
+const API_KEY = import.meta.env.VITE_REST_COUNTRIES_API_KEY;
 
-export function useCountry (cca3: string | undefined){
-    return useQuery({
-        queryKey: ["country", cca3],
-        enabled: Boolean (cca3),
-        queryFn: async () => {
-            const url = `${BASE}/${cca3}?fields=name,flags,region,subregion,capital,population,languages,currencies,capitalInfo,latlng,cca3`;
-            const res = await axios.get(url);
+export function useCountry(cca3: string | undefined) {
+  return useQuery({
+    queryKey: ["country", cca3],
+    enabled: Boolean(cca3),
 
-            return Array.isArray(res.data) ? res.data[0] : res.data;
+    queryFn: async () => {
+      const { data } = await axios.get(API_URL, {
+        headers: {
+          Authorization: `Bearer ${API_KEY}`,
         },
-        staleTime:1000 * 60 * 5, //5 minutes 
+        params: {
+          limit: 1,
+          "codes.alpha_3": cca3,
+        },
+      });
 
-        }
-    )
-};
+      const country = data.data.objects?.[0];
 
+      if (!country) {
+        throw new Error("Country not found");
+      }
+
+      return {
+        name: {
+          common: country.names?.common ?? "",
+          official: country.names?.official ?? "",
+        },
+
+        flags: {
+          png: country.flag?.url_png ?? "",
+          svg: country.flag?.url_svg ?? "",
+        },
+
+        region: country.region ?? "",
+        subregion: country.subregion ?? "",
+
+        capital: country.capitals?.map((c: any) => c.name) ?? [],
+
+        population: country.population ?? 0,
+
+        languages: country.languages
+          ? Object.fromEntries(
+              country.languages.map((lang: any, index: number) => [
+                `lang${index}`,
+                lang.name,
+              ])
+            )
+          : {},
+
+        currencies: country.currencies
+          ? Object.fromEntries(
+              country.currencies.map((currency: any) => [
+                currency.code,
+                {
+                  name: currency.name,
+                  symbol: currency.symbol,
+                },
+              ])
+            )
+          : {},
+
+        capitalInfo: {
+          latlng: country.capitals?.[0]?.coordinates
+            ? [
+                country.capitals[0].coordinates.lat,
+                country.capitals[0].coordinates.lng,
+              ]
+            : [],
+        },
+
+        latlng: country.coordinates
+          ? [country.coordinates.lat, country.coordinates.lng]
+          : [],
+
+        cca3: country.codes?.alpha_3 ?? "",
+      };
+    },
+
+    staleTime: 1000 * 60 * 5,
+  });
+}
